f8e02245d0111708d85b1fa3aa0c2f496c7831e3
phuoctai <phuoctai9293@gmail.com>
2021-04-02 12:07:58 +0700

    commit thuc hanh bai 7

---
+++ _07_bien_kieu_du_lieu_toan_tu/thuc_hanh/toan_tu1.js
@@ -0,0 +1,7 @@
+
+let inputWidth = prompt("nhap vao chieu rong");
+let inputHeight = prompt("nhap vao chieu cao");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area = width*height;
+document.write("dien tich la: "+ area);
